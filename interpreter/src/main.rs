f536fdc6de8dc74b3ceb2c464d12124c295ccafa
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-09-26 16:50:18 -0300

    represent instructions by a enum

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,26 +1,49 @@
 use std::io::Read;
 
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Instruction {
+    Increase,
+    Decrease,
+    MoveRight,
+    MoveLeft,
+    Input,
+    Output,
+    JumpRight,
+    JumpLeft,
+}
+
 fn main() -> std::io::Result<()> {
     let file_name = std::env::args().nth(1).unwrap();
     let source = std::fs::read(&file_name)?;
 
-    let source: Vec<u8> = source
+    let source: Vec<Instruction> = source
         .into_iter()
-        .filter(|b| [b'+', b'-', b'.', b',', b'>', b'<', b'[', b']'].contains(b))
+        .filter_map(|b| match b {
+            b'+' => Some(Instruction::Increase),
+            b'-' => Some(Instruction::Decrease),
+            b'.' => Some(Instruction::Output),
+            b',' => Some(Instruction::Input),
+            b'>' => Some(Instruction::MoveRight),
+            b'<' => Some(Instruction::MoveLeft),
+            b'[' => Some(Instruction::JumpRight),
+            b']' => Some(Instruction::JumpLeft),
+            _ => None,
+        })
         .collect();
 
     let mut pointer = 0;
     let mut program_counter = 0;
     let mut memory = [0u8; 30_000];
     'program: loop {
+        use Instruction::*;
         match source[program_counter] {
-            b'+' => memory[pointer] = memory[pointer].wrapping_add(1),
-            b'-' => memory[pointer] = memory[pointer].wrapping_sub(1),
-            b'.' => print!("{}", memory[pointer] as char),
-            b',' => std::io::stdin().read_exact(&mut memory[pointer..pointer + 1])?,
-            b'>' => pointer = (pointer + 1) % memory.len(),
-            b'<' => pointer = (pointer + memory.len() - 1) % memory.len(),
-            b'[' => {
+            Increase => memory[pointer] = memory[pointer].wrapping_add(1),
+            Decrease => memory[pointer] = memory[pointer].wrapping_sub(1),
+            Output => print!("{}", memory[pointer] as char),
+            Input => std::io::stdin().read_exact(&mut memory[pointer..pointer + 1])?,
+            MoveRight => pointer = (pointer + 1) % memory.len(),
+            MoveLeft => pointer = (pointer + memory.len() - 1) % memory.len(),
+            JumpRight => {
                 if memory[pointer] == 0 {
                     let mut deep = 1;
                     loop {
@@ -28,10 +51,10 @@ fn main() -> std::io::Result<()> {
                             break 'program;
                         }
                         program_counter += 1;
-                        if source[program_counter] == b'[' {
+                        if source[program_counter] == JumpRight {
                             deep += 1;
                         }
-                        if source[program_counter] == b']' {
+                        if source[program_counter] == JumpLeft {
                             deep -= 1;
                         }
                         if deep == 0 {
@@ -40,7 +63,7 @@ fn main() -> std::io::Result<()> {
                     }
                 }
             }
-            b']' => {
+            JumpLeft => {
                 if memory[pointer] != 0 {
                     let mut deep = 1;
                     loop {
@@ -48,10 +71,10 @@ fn main() -> std::io::Result<()> {
                             break 'program;
                         }
                         program_counter -= 1;
-                        if source[program_counter] == b']' {
+                        if source[program_counter] == JumpLeft {
                             deep += 1;
                         }
-                        if source[program_counter] == b'[' {
+                        if source[program_counter] == JumpRight {
                             deep -= 1;
                         }
                         if deep == 0 {
@@ -60,7 +83,6 @@ fn main() -> std::io::Result<()> {
                     }
                 }
             }
-            _ => unreachable!(),
         }
         program_counter += 1;
 
