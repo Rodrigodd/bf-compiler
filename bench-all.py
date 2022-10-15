0e347258bdac84ed03fc7958c15b3ad870f8649c
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-10-15 15:01:19 -0300

    bench-all.py: don't compile each commit many times

--- bench-all.py
+++ bench-all.py
@@ -5,7 +5,7 @@ import re
 
 steps = [
     ("basic", "689bc2a"),
-    ("precompute jumps", "0be6f60"),
+    ("precomputejumps", "0be6f60"),
     ("Add", "1ed437e"),
     ("Move", "cbd8ba5"),
     ("Clear", "bb4ff06"),
@@ -16,8 +16,6 @@ steps = [
 p = re.compile(b"real\t(\d+)m(\d+.\d+)s")
 
 def time(command):
-    os.system("git checkout " + commit)
-    os.system("cargo build -p bf-optimized --release")
     out = subprocess.run(['bash', '-c',
         f"time ( {command} ) "],
          stderr=subprocess.PIPE)
@@ -33,15 +31,25 @@ def time(command):
 
     return t
 
+# compile each commit
+os.system('mkdir bench_programs')
+for name, commit in steps:
+    if os.system(f'git checkout {commit}') != 0:
+        raise('git checkout failed')
+    os.system('cargo build -p bf-optimized --release')
+    os.system(f'mv ./target/release/bf-optimized ./bench_programs/{name}')
+
 for interation in range(0,10):
+    print(f'interation {interation}')
     time_csv = open(f'times/times{interation}.csv', 'w')
 
     time_csv.write("change,factor.bf,mandelbrot.bf\n")
     for name, commit in steps:
+        print(f'running for {name}')
         text = name + ","
 
-        t1 = time('echo 179424691 | ./target/release/bf-optimized ./programs/factor.bf')
-        t2 = time('./target/release/bf-optimized ./programs/mandelbrot.bf')
+        t1 = time(f'echo 179424691 | ./bench_programs/{name} ./programs/factor.bf')
+        t2 = time(f'./bench_programs/{name} ./programs/mandelbrot.bf')
 
         text = text + str(t1) + "," + str(t2) + "\n"
 
