75ca9eb382db67b365604e596645342a252c3bf0
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-12-22 13:54:45 -0300

    link compiled code to a lib written in rust

---
+++ singlepass-compiler/bf_lib.rs
@@ -0,0 +1,47 @@
+use std::io::{Read, Write};
+
+#[no_mangle]
+pub extern "sysv64" fn bf_write(value: u8) -> *mut std::io::Error {
+    // Writing a non-UTF-8 byte sequence on Windows error out.
+    if cfg!(target_os = "windows") && value >= 128 {
+        return std::ptr::null_mut();
+    }
+
+    let mut stdout = std::io::stdout().lock();
+
+    let result = stdout.write_all(&[value]).and_then(|_| stdout.flush());
+
+    match result {
+        Err(err) => {
+            eprintln!("IO error: {}", err);
+            std::process::exit(1);
+        }
+        _ => std::ptr::null_mut(),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "sysv64" fn bf_read(buf: *mut u8) -> *mut std::io::Error {
+    let mut stdin = std::io::stdin().lock();
+    loop {
+        let mut value = 0;
+        let err = stdin.read_exact(std::slice::from_mut(&mut value));
+
+        if let Err(err) = err {
+            if err.kind() != std::io::ErrorKind::UnexpectedEof {
+                eprintln!("IO error: {}", err);
+                std::process::exit(1);
+            }
+            value = 0;
+        }
+
+        // ignore CR from Window's CRLF
+        if cfg!(target_os = "windows") && value == b'\r' {
+            continue;
+        }
+
+        *buf = value;
+
+        return std::ptr::null_mut();
+    }
+}
--- singlepass-compiler/src/main.rs
+++ singlepass-compiler/src/main.rs
@@ -3,14 +3,7 @@ use std::process::ExitCode;
 use dynasmrt::{dynasm, x64::X64Relocation, DynasmApi, DynasmLabelApi, VecAssembler};
 
 use object::{
-    elf::{
-        ELFOSABI_SYSV, EM_X86_64, ET_EXEC, PF_R, PF_X, PT_LOAD, SHF_ALLOC, SHF_EXECINSTR,
-        SHT_PROGBITS,
-    },
-    write::{
-        elf::{FileHeader, ProgramHeader},
-        Symbol,
-    },
+    write::{Relocation, Symbol},
     SymbolFlags,
 };
 
@@ -18,6 +11,8 @@ struct UnbalancedBrackets(char, usize);
 
 struct Program {
     code: Vec<u8>,
+    write_relocations: Vec<usize>,
+    read_relocations: Vec<usize>,
 }
 impl Program {
     fn new(source: &[u8]) -> Result<Program, UnbalancedBrackets> {
@@ -36,6 +31,9 @@ impl Program {
             ; mov r12, rsp
         };
 
+        let mut write_relocations = Vec::new();
+        let mut read_relocations = Vec::new();
+
         let mut bracket_stack = Vec::new();
 
         for b in source {
@@ -50,20 +48,17 @@ impl Program {
                 },
                 b'.' => dynasm! { code
                     ; .arch x64
-                    ; mov rax, 1 // write syscall
-                    ; mov rdi, 1 // stdout's file descriptor
-                    ; lea rsi, [r12 + r13] // buf address
-                    ; mov rdx, 1           // length
-                    ; syscall
+                    ; mov rdi, [r12 + r13] // cell value
+                    ; call DWORD 0
+                    ;; write_relocations.push(code.offset().0 - 4)
                 },
                 b',' => dynasm! { code
                     ; .arch x64
-                    ; mov rax, 0 // read syscall
-                    ; mov rdi, 0 // stdin's file descriptor
-                    ; lea rsi, [r12 + r13] // buf address
-                    ; mov rdx, 1           // length
-                    ; syscall
+                    ; lea rdi, [r12 + r13] // cell address
+                    ; call DWORD 0
+                    ;; read_relocations.push(code.offset().0 - 4)
                 },
+
                 b'<' => dynasm! { code
                     ; .arch x64
                     ; sub r13, 1
@@ -113,7 +108,6 @@ impl Program {
         dynasm! { code
             ; .arch x64
             ; xor rax, rax
-            ; ->exit:
             ; mov rdi, rax // exit error code
             ; mov rax, 60 // exit syscall
             ; syscall
@@ -121,6 +115,8 @@ impl Program {
 
         Ok(Program {
             code: code.finalize().unwrap(),
+            write_relocations,
+            read_relocations,
         })
     }
 
@@ -141,88 +137,61 @@ impl Program {
             section: object::write::SymbolSection::Undefined,
             flags: SymbolFlags::None,
         });
+        let bf_write = obj.add_symbol(Symbol {
+            name: b"bf_write".to_vec(),
+            value: 0,
+            size: 0,
+            kind: object::SymbolKind::Text,
+            scope: object::SymbolScope::Linkage,
+            weak: false,
+            section: object::write::SymbolSection::Undefined,
+            flags: SymbolFlags::None,
+        });
+        let bf_read = obj.add_symbol(Symbol {
+            name: b"bf_read".to_vec(),
+            value: 0,
+            size: 0,
+            kind: object::SymbolKind::Text,
+            scope: object::SymbolScope::Linkage,
+            weak: false,
+            section: object::write::SymbolSection::Undefined,
+            flags: SymbolFlags::None,
+        });
 
         let text = obj.section_id(object::write::StandardSection::Text);
         obj.add_symbol_data(start, text, &self.code, 16);
+
+        for offset in self.read_relocations.iter().copied() {
+            obj.add_relocation(
+                text,
+                Relocation {
+                    offset: offset as u64,
+                    size: 32,
+                    kind: object::RelocationKind::Relative,
+                    encoding: object::RelocationEncoding::Generic,
+                    symbol: bf_read,
+                    addend: -4,
+                },
+            )
+            .unwrap();
+        }
+        for offset in self.write_relocations.iter().copied() {
+            obj.add_relocation(
+                text,
+                Relocation {
+                    offset: offset as u64,
+                    size: 32,
+                    kind: object::RelocationKind::Relative,
+                    encoding: object::RelocationEncoding::Generic,
+                    symbol: bf_write,
+                    addend: -4,
+                },
+            )
+            .unwrap();
+        }
+
         let mut out = Vec::new();
         obj.emit(&mut out).unwrap();
-
-        out
-    }
-
-    fn to_elf_executable(&self) -> Vec<u8> {
-        let mut out = Vec::new();
-        let mut writer =
-            object::write::elf::Writer::new(object::Endianness::Little, true, &mut out);
-
-        let text_name = writer.add_section_name(b".text");
-        let _text_section = writer.reserve_section_index();
-
-        writer.reserve_file_header();
-
-        writer.reserve_program_headers(1);
-
-        writer.reserve_strtab_section_index();
-        writer.reserve_strtab();
-
-        writer.reserve_shstrtab_section_index();
-        writer.reserve_shstrtab();
-
-        writer.reserve_section_headers();
-
-        let text_offset = writer.reserve(self.code.len(), 16);
-
-        const PAGE_SIZE: u64 = 0x1000;
-
-        writer
-            .write_file_header(&FileHeader {
-                os_abi: ELFOSABI_SYSV,
-                abi_version: 0,
-                e_type: ET_EXEC,
-                e_machine: EM_X86_64,
-                e_entry: 0x400000 + (text_offset as u64 % PAGE_SIZE),
-                e_flags: 0,
-            })
-            .unwrap();
-
-        writer.write_align_program_headers();
-        writer.write_program_header(&ProgramHeader {
-            p_type: PT_LOAD,
-            p_flags: PF_R | PF_X,
-            p_offset: text_offset as u64,
-            p_vaddr: 0x400000 + (text_offset as u64 % PAGE_SIZE),
-            p_paddr: 0,
-            p_filesz: self.code.len() as u64,
-            p_memsz: self.code.len() as u64,
-            p_align: PAGE_SIZE,
-        });
-
-        writer.write_strtab();
-
-        writer.write_shstrtab();
-
-        writer.write_null_section_header();
-
-        writer.write_section_header(&object::write::elf::SectionHeader {
-            name: Some(text_name),
-            sh_type: SHT_PROGBITS,
-            sh_flags: (SHF_ALLOC | SHF_EXECINSTR) as u64,
-            sh_addr: 0x400000,
-            sh_offset: text_offset as u64,
-            sh_size: self.code.len() as u64,
-            sh_link: 0,
-            sh_info: 0,
-            sh_addralign: 16,
-            sh_entsize: 0,
-        });
-
-        writer.write_strtab_section_header();
-        writer.write_shstrtab_section_header();
-
-        writer.write_align(16);
-        writer.write(&self.code);
-
-        assert_eq!(writer.reserved_len(), writer.len());
 
         out
     }
@@ -264,11 +233,6 @@ fn main() -> ExitCode {
             let output_name = std::path::Path::new(&output_name).with_extension("o");
             let obj = program.to_elf_object();
             std::fs::write(output_name, obj).unwrap();
-        }
-        "-x" => {
-            let output_name = std::path::Path::new(&output_name).with_extension("");
-            let exe = program.to_elf_executable();
-            std::fs::write(output_name, exe).unwrap();
         }
         arg => panic!("unknown arg {arg}"),
     }
